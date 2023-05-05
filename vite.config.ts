73ce1720fd6d9440f560f3395d87707cdda5ff0f
kim-limit <rlawogks2468@naver.com>
2023-05-05 16:04:15 +0900

    feat: 기본 환경설정 완료

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: "@src", replacement: resolve(__dirname, "src") },
+      {
+        find: "@components",
+        replacement: resolve(__dirname, "src/components"),
+      },
+    ],
+  },
+
+  plugins: [react(), tsconfigPaths()],
+});
