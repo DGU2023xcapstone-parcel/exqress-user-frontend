d435aac12f45d05a1d468155e3024540f6c53613
kim-limit <rlawogks2468@naver.com>
2023-05-07 21:38:34 +0900

    feat: queryClient 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "@emotion/react";
 import AppRouter from "./routes/AppRouter";
 import GlobalStyle from "./style/globalStyle";
 import { theme } from "./style/theme";
 
+const queryClient = new QueryClient();
 function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <AppRouter />
+        <QueryClientProvider client={queryClient}>
+          <GlobalStyle />
+          <AppRouter />
+        </QueryClientProvider>
       </ThemeProvider>
     </>
   );
