ba8cba810c0b0a2f53ca7df44ba937abaf2dfbb6
kim-limit <rlawogks2468@naver.com>
2023-05-07 21:35:15 +0900

    feat: 상수 정의

---
+++ src/constants/oAuth.ts
@@ -0,0 +1,3 @@
+const REST_API_KEY = "2b15fd82639d4abfe6c447dcd536e437";
+const REDIRECT_URI = "http://localhost:5173/signin/kakao";
+const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
---
+++ src/constants/url.ts
@@ -0,0 +1,1 @@
+export const API_URL = "http://10.100.78.222:8080" as const;
